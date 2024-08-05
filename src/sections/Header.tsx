2b94cee52703b2e22377a8413ee8cd80d87d5033
Nik Lakhani <mr.niklakhani@gmail.com>
2024-08-05 10:05:59 +0530

    logo ticker set

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,8 +5,10 @@ import Button from '@/components/Button'
 export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
           <div className="">
             <div className="border border-white/15 h-10 w-10 rounded-lg inline-flex justify-center items-center">
               <LogoIcon className="h-8 w-8" />
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,25 @@
+import acmeLogo from '@/assets/logo-acme.png'
+import apexLogo from '@/assets/logo-apex.png'
+import celestialLogo from '@/assets/logo-celestial.png'
+import quantumLogo from '@/assets/logo-quantum.png'
+import pulseLogo from '@/assets/logo-pulse.png'
+import echoLogo from '@/assets/logo-echo.png'
+
 export const LogoTicker = () => {
-  return <section>LogoTicker</section>;
-};
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <div className="flex items-center gap-5">
+          <h2 className="flex-1 md:flex-none">Trusted by top innovative teams</h2>
+          <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <div className="flex flex-none gap-14">
+              {[acmeLogo, pulseLogo, echoLogo, celestialLogo, apexLogo, quantumLogo].map((logo, index) => (
+                <img src={logo.src} key={index} alt="" className="h-6 w-auto" />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
