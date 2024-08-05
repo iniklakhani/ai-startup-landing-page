da02605a43992fbe43993a4b6be8bced35127d3e
Nik Lakhani <mr.niklakhani@gmail.com>
2024-08-05 12:25:37 +0530

    set feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/sections/Header'
 import { Hero } from '@/sections/Hero'
 import { LogoTicker } from '@/sections/LogoTicker'
+import { Features } from '@/sections/Features'
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <LogoTicker />
+      <Features />
     </>
   )
 }
--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,30 +1,70 @@
+'use client'
+import ProductImage from '@/assets/product-image.png'
+import { DotLottiePlayer } from '@dotlottie/react-player'
+
 const tabs = [
   {
-    icon: "/assets/lottie/vroom.lottie",
-    title: "User-friendly dashboard",
+    icon: '/assets/lottie/vroom.lottie',
+    title: 'User-friendly dashboard',
     isNew: false,
     backgroundPositionX: 0,
     backgroundPositionY: 0,
     backgroundSizeX: 150,
   },
   {
-    icon: "/assets/lottie/click.lottie",
-    title: "One-click optimization",
+    icon: '/assets/lottie/click.lottie',
+    title: 'One-click optimization',
     isNew: false,
     backgroundPositionX: 98,
     backgroundPositionY: 100,
     backgroundSizeX: 135,
   },
   {
-    icon: "/assets/lottie/stars.lottie",
-    title: "Smart keyword generator",
+    icon: '/assets/lottie/stars.lottie',
+    title: 'Smart keyword generator',
     isNew: true,
     backgroundPositionX: 100,
     backgroundPositionY: 27,
     backgroundSizeX: 177,
   },
-];
+]
 
 export const Features = () => {
-  return <section>Features</section>;
-};
+  return (
+    <section className="py-20 md:py-24">
+      <div className="container">
+        <h2 className="text-5xl md:text-6xl font-medium text-center tracking-tighter">Elevate your SEO efforts.</h2>
+        <p className="text-white/70 text-lg md:text-xl max-w-2xl mx-auto tracking-tighter text-center mt-5">
+          From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach
+          SEO.
+        </p>
+
+        <div className="mt-10 flex flex-col lg:flex-row gap-3">
+          {tabs.map((tab, _) => (
+            <div
+              className="border border-white/15 flex p-2.5 rounded-xl gap-2.5 items-center lg:flex-1"
+              key={tab.title}
+            >
+              <div className="h-12 w-12 border border-white/15 rounded-lg inline-flex items-center justify-center">
+                <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay />
+              </div>
+              <div className="font-medium">{tab.title}</div>
+              {tab.isNew && (
+                <div className="text-xs rounded-full px-2 py-0.5 bg-[#8c44ff] text-black font-semibold">new</div>
+              )}
+            </div>
+          ))}
+        </div>
+
+        <div className="border border-white/20 p-2.5 rounded-xl mt-3">
+          <div
+            className="aspect-video bg-cover border border-white/20 rounded-lg"
+            style={{
+              backgroundImage: `url(${ProductImage.src})`,
+            }}
+          ></div>
+        </div>
+      </div>
+    </section>
+  )
+}
