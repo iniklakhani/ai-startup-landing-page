7c017004644a67f4fd6545d33fe92ecb72b99247
Nik Lakhani <mr.niklakhani@gmail.com>
2024-08-05 08:41:53 +0530

    make header nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Header } from '@/sections/Header'
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return <Header />
 }
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,48 @@
+import LogoIcon from '@/assets/logo.svg'
+import MenuIcon from '@/assets/icon-menu.svg'
+
 export const Header = () => {
-  return <header>Header</header>;
-};
+  return (
+    <header className="py-4 border-b border-white/15 md:border-none">
+      <div className="container">
+        <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 rounded-xl max-w-2xl mx-auto">
+          <div className="">
+            <div className="border border-white/15 h-10 w-10 rounded-lg inline-flex justify-center items-center">
+              <LogoIcon className="h-8 w-8" />
+            </div>
+          </div>
+          <div className="hidden md:block">
+            <nav className="flex gap-8 text-sm">
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Features
+              </a>
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Developers
+              </a>
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Pricing
+              </a>
+              <a href="#" className="text-white/70 hover:text-white transition">
+                Changelog
+              </a>
+            </nav>
+          </div>
+          <div className="flex gap-4 items-center">
+            <button
+              className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12x_0px_#8c45ff]"
+              style={{ boxShadow: '0 0 12px #8c45ff' }}
+            >
+              <div className="absolute inset-0 rounded-lg">
+                <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-gradient(to_bottom,black,transparent)]"></div>
+                <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)]"></div>
+                <div className="absolute inset-0 shadow-[0px_0px_10px_0px_rgba(140,69,255,0.7)_inset] rounded-lg"></div>
+              </div>
+              <span>Join waitlist</span>
+            </button>
+            <MenuIcon className="md:hidden" />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
